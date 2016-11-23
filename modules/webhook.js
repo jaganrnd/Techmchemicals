073767b1ab07be9fa05d914916678ccaf402dfe7
jaganrnd <jaganrnd@gmail.com>
2016-11-23 18:59:55 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -58,7 +58,7 @@ let getArticles = (incomingtext) => {
        
 	
         request({
-            url: `https://ap2.salesforce.com/services/data/v36.0/parameterizedSearch/?q%3Di+need+to+know+my+balance+%26sobject%3DKnowledgeArticleVersion+%26KnowledgeArticleVersion.fields%3DTitle%2CSummary+%26KnowledgeArticleVersion.where+publishstatus%3D%27online%27+and+language%3D%27en_US%27`,
+            url: 'https://ap2.salesforce.com/services/data/v36.0/parameterizedSearch/?q%3Di+need+to+know+my+balance+%26sobject%3DKnowledgeArticleVersion+%26KnowledgeArticleVersion.fields%3DTitle%2CSummary+%26KnowledgeArticleVersion.where+publishstatus%3D%27online%27+and+language%3D%27en_US%27',
             method: 'GET',		
         }, (error, response) => {
             if (error) {
